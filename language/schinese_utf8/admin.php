d4dff4bc233e3ac5eea12ddad9f77777b229132b
srmcatee <srmcatee@9b3c414f-5b12-0410-97f9-d54092e68bf8>
2008-12-29 13:12:13 +0000

    Add Simple Chinese UTF8 language files.

---
+++ language/schinese_utf8/admin.php
@@ -0,0 +1,42 @@
+﻿<?php
+define("_oscgiv_ADMINTITLE","OSC 捐献者 - 管理");
+//
+
+//
+define("_oscgiv_DB_CREATED","创建表成功");
+
+define("_oscgiv_admin_access_denied","访问管理页面被拒绝");
+
+define("_oscgiv_permissions_view","访问权限");
+define("_oscgiv_permissions_view_desc","查看贡献权限");
+
+define("_oscgiv_permissions_modify","修改/创建权限");
+define("_oscgiv_permissions_modify_desc","修改/创建捐助权限");
+
+define("_oscgiv_permissions_admin","管理权限");
+define("_oscgiv_permissions_admin_desc","管理捐助权限");
+
+define("_oscgiv_yearenddonationletter_text","捐助者年终信正文<br><small>只支持png格式图片</small>");
+define("_oscgiv_settings_title","OSC捐助 管理");
+
+define("_oscgiv_letterfields","自定义年终信发送范围");
+define("_oscgiv_letterfields_instructions","使用下列领域定义捐助信发送范围");
+define("_oscgiv_letterfields_fields","<ul><li>[捐助人姓名]: 捐助人姓名</li><li>[日期]: 信件日期</li><li>[捐助表]: 插入单个捐助人年度捐助表格</li>
+<li>[捐助人住址]:捐助人地址</li>
+<li>[教堂名]: 教堂名，从员管OSC成员系统设置中获取</li>
+<li>[年]:选择捐助报告的年份</li>
+<li>[捐助总量]: 所选年度捐助总量</li></ul>");
+
+define("_oscgiv_yearendletter_savesuccess","年终信设置更改成功！");
+
+define("_oscgiv_edit","编辑");
+define("_oscgiv_fundname","基金名");
+define("_oscgiv_fundlist","基金列表");
+define("_oscgiv_addfund","添加基金");
+define("_oscgiv_funds","基金");
+
+define("_oscgiv_fundadmin_TITLE","基金详细");
+
+define("_oscgiv_fundcreatefailure","创建基金失败");
+define("_oscgiv_fundupdatefailure","基金更新失败");
+?>
---
+++ language/schinese_utf8/blocks.php
@@ -0,0 +1,5 @@
+﻿<?php
+// Blocks
+define("_oscgiv_nav_block_title","OSC捐助 块");
+//
+?>
